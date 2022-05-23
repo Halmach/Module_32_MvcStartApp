f68599e963324e8463b515e84d8c54875972ea66
Khalmatov <large000@yandex.ru>
2022-05-23 23:46:33 +0300

    Передача данных в представление

---
+++ MvcStartApp/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcStartApp.Models;
+using MvcStartApp.Models.Db;
+using MvcStartApp.Models.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcStartApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        // ссылка на репозиторий
+        private readonly IBlogRepository _repo;
+
+
+        public UsersController(IBlogRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var authors = await _repo.GetUsers();
+            return View(authors);
+        }
+
+    }
+}
