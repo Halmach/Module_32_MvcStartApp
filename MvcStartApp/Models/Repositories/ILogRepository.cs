874f0259d0d69969db077b03a84a60cfff0c13d0
Khalmatov <large000@yandex.ru>
2022-05-25 00:13:23 +0300

    Работает логирование в БД, для доступа к странице для просмотра логов - нажать на панели сверху ссылку Visit logs

---
+++ MvcStartApp/Models/Repositories/ILogRepository.cs
@@ -0,0 +1,11 @@
+﻿using MvcStartApp.Models.Db;
+using System.Threading.Tasks;
+
+namespace MvcStartApp.Models.Repositories
+{
+    public interface ILogRepository
+    {
+        Task AddLog(Request request);
+        Task<Request[]> GetRequests();
+    }
+}
---
+++ MvcStartApp/Models/Repositories/LogRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcStartApp.Models.Db;
+using System;
+using System.Threading.Tasks;
+
+namespace MvcStartApp.Models.Repositories
+{
+    public class LogRepository : ILogRepository
+    {
+        // ссылка на контекст
+        private readonly BlogContext _context;
+
+        public LogRepository(BlogContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddLog(Request request)
+        {
+           request.Id = Guid.NewGuid();
+           request.Date = DateTime.Now;
+
+            // Добавление пользователя
+            var entry = _context.Entry(request);
+            if (entry.State == EntityState.Detached)
+                await _context.Requests.AddAsync(request);
+
+            // Сохранение изенений
+            await _context.SaveChangesAsync();
+
+        }
+
+        public Task<Request[]> GetRequests()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
